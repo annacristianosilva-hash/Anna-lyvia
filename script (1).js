0a1adaee11c41917236e2d78452a3a0956d01c43
annacristianosilva-hash <anna.cristiano.silva@escola.pr.gov.br>
2026-06-12 12:23:35 -0300

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,21 @@
+function appendValue(value) {
+  const display = document.getElementById('display');
+  if (display.value === '0') {
+    display.value = value;
+  } else {
+    display.value += value;
+  }
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '0';
+}
+
+function calculateResult() {
+  const display = document.getElementById('display');
+  try {
+    display.value = eval(display.value);
+  } catch (e) {
+    display.value = 'Erro';
+  }
+}
